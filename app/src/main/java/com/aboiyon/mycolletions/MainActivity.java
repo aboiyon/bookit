5dd95e17af30958eb7dd8aacd182e960817eb903
kiprotich <kiprotichmichael570@gmail.com>
2023-04-29 19:00:05 +0300

    feat: add intent to navigate to books activity

--- app/src/main/java/com/aboiyon/mycolletions/MainActivity.java
+++ app/src/main/java/com/aboiyon/mycolletions/MainActivity.java
@@ -1,6 +1,8 @@
 package com.aboiyon.mycolletions;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mFindBooksButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, BooksActivity.class);
+                startActivity(intent);
             }
         });
     }
