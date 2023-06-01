ad84e45e66e2bfe17e6eb1d28dec8399bf00e22c
kiprotich <kiprotichmichael570@gmail.com>
2023-06-01 16:54:57 +0300

    feat: bundle information with parcelable

--- app/src/main/java/com/aboiyon/mycolletions/ui/BookDetailFragment.java
+++ app/src/main/java/com/aboiyon/mycolletions/ui/BookDetailFragment.java
@@ -1,13 +1,17 @@
 package com.aboiyon.mycolletions.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.aboiyon.mycolletions.R;
+import com.aboiyon.mycolletions.databinding.FragmentBookDetailBinding;
+import com.aboiyon.mycolletions.models.Book;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,50 +20,50 @@ import com.aboiyon.mycolletions.R;
  */
 public class BookDetailFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentBookDetailBinding binding;
+    private Book mBook;
 
     public BookDetailFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BookDetailFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static BookDetailFragment newInstance(String param1, String param2) {
-        BookDetailFragment fragment = new BookDetailFragment();
+    public static BookDetailFragment newInstance(Book book) {
+        BookDetailFragment bookDetailFragment = new BookDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putParcelable("book", book);
+        bookDetailFragment.setArguments(args);
+        return bookDetailFragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        assert getArguments() != null;
+        mBook = getArguments().getParcelable("book");
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_detail, container, false);
+        // Inflate the layout for this fragment using view binding
+        binding = FragmentBookDetailBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        Picasso.get().load(mBook.getImage()).into(binding.bookImageView);
+
+        binding.bookTitleTextView.setText(mBook.getTitle());
+        binding.subtitleTextView.setText(mBook.getSubtitle());
+        binding.priceTextView.setText(mBook.getPrice());
+        binding.isbnTextView.setText(mBook.getIsbn13());
+        binding.urlTextView.setText(mBook.getUrl());
+        return view;
+    }
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        binding = null; // Clear the binding reference to avoid memory leaks
     }
 }
