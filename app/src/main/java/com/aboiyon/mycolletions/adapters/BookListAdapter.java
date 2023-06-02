d3f1aeb15ad1944730ca246a1cb1b256ffb24a98
kiprotich <kiprotichmichael570@gmail.com>
2023-06-02 14:39:29 +0300

    feat: add click listener to click book in recycler view and navigate to individual book's detail page which is populated with information about that specific book

--- app/src/main/java/com/aboiyon/mycolletions/adapters/BookListAdapter.java
+++ app/src/main/java/com/aboiyon/mycolletions/adapters/BookListAdapter.java
@@ -1,13 +1,16 @@
 package com.aboiyon.mycolletions.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.aboiyon.mycolletions.databinding.BookListItemBinding;
 import com.aboiyon.mycolletions.models.Book;
+import com.aboiyon.mycolletions.ui.BookDetailActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -20,18 +23,32 @@ public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.BookVi
         mContext = context;
         mBooks = books;
     }
-    public class BookViewHolder extends RecyclerView.ViewHolder {
+    public class BookViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private BookListItemBinding mBinding;
+        private Context mContext;
+        private List<Book> mBooks;
 
         public BookViewHolder(BookListItemBinding binding){
             super(binding.getRoot());
             mBinding = binding;
+            mContext = itemView.getContext();
+
+            itemView.setOnClickListener(this);
         }
         public void bindBook(Book book){
             Picasso.get().load(book.getImage()).into(mBinding.bookImageView);
             mBinding.bookNameTextView.setText(book.getTitle());
             mBinding.categoryTextView.setText(book.getSubtitle());
             mBinding.isbnTextView.setText(book.getIsbn13());
+        }
+
+        @Override
+        public void onClick(View view) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, BookDetailActivity.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("books", (CharSequence) mBooks);
+            mContext.startActivity(intent);
         }
     }
     @Override
